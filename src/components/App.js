24e5044bf148d548583d34764aa8feac88dc5b46
Tochine <toshineluv99@yahoo.com>
2020-02-10 23:49:46 +0100

    ui mobile friendly

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import "./Styles.css";
-import book1 from "./book1.jpg";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -17,17 +16,13 @@ function App() {
       <div className="header">
         <h2>Rick & Morty</h2>
       </div>
-      <section>
-        <div className="container">
-          <div className="card-wrapper">
-            {movies.map((movie, mvId) => (
-              <div className="card" key={mvId}>
-                <img src={movie.image} alt="images of rick and morty" />
-                <p className="imgName">{movie.name}</p>
-              </div>
-            ))}
+      <section className="gallery">
+        {movies.map((movie, mvId) => (
+          <div key={mvId}>
+            <img src={movie.image} alt="images of rick and morty" />
+            <p className="imgName">{movie.name}</p>
           </div>
-        </div>
+        ))}
       </section>
     </>
   );
